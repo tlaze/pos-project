3251cf9a8d9f187508673d20a0a4874cfd060e22
Tom <tomlazore@gmail.com>
2025-07-17 13:53:07 -0400

    items persist on refresh using piniaPersistedState

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from './App.vue'
 import './assets/tailwind.css';
 import router from './router';
 import { createPinia } from 'pinia';
+import piniaPersistedState from 'pinia-plugin-persistedstate'
 
 const app = createApp(App)
 const pinia = createPinia();
+pinia.use(piniaPersistedState)
 app.use(pinia);
 app.use(router);
 app.mount('#app');
--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -37,4 +37,6 @@ export const useCartStore = defineStore('cart', () => {
   }
 
   return { items, total, addToCart, increment, decrement, clearCart };
+}, {
+    persist:true
 });
